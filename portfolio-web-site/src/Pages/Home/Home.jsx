e2f6f21d1f9bb7226bcce742e9945e5b218b777c
Artem <a.bychkoff1@yandex.ru>
2023-02-03 14:55:53 +0300

    install library

---
+++ portfolio-web-site/src/Pages/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+
+const Home = () => {
+  return (
+    <Container >
+      <Row >
+        <Col>
+        <div className="links">
+        <i class="bi bi-github"></i>
+        </div>
+          </Col>
+        <Col>
+        <div className="langAndThemes">
+          qwe
+        </div>
+        </Col>
+      </Row>
+      {/* <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row> */}
+    </Container>
+  )
+}
+
+export default Home
