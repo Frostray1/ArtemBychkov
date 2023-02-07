b9590f7db20ec79805dfa7c38eb234793f8ef836
Artem <a.bychkoff1@yandex.ru>
2023-02-07 18:05:01 +0300

    add adaptive design

--- portfolio-web-site/src/Components/LybraryCard.jsx
+++ portfolio-web-site/src/Components/LybraryCard.jsx
@@ -2,20 +2,20 @@ import { Col, Row } from "react-bootstrap";
 import React from "react";
 
 const LybraryCard = (props) => {
-  console.log({ props });
+  console.log(props.item.info);
   //   <h5>{props.item.name}</h5>
   return (
     <div className="blockCard">
       <Row>
         <Col>
-          <img src={props.item.imgLink} alt="" />
+          <img src={props.item.imgLink} alt="logo" />
         </Col>
         <Col>
         <div className="blockCard__info">
           <h3>{props.item.title}</h3>
+          
           <p>
             {props.item.info}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum adipisci alias quisquam delectus, officia voluptates voluptate voluptas recusandae eligendi eaque illo magni inventore consequuntur velit exercitationem dicta magnam. Possimus.
           </p>
           <Row className="blockCard__links">
           <Col >
--- portfolio-web-site/src/listLibrary.js
+++ portfolio-web-site/src/listLibrary.js
@@ -1,24 +1,24 @@
 export const card = [
     {
       title: 'To-do List',
-      imgLink: 'https://user-images.githubusercontent.com/48648751/215086300-1f7cc837-ddfb-4c9a-bc28-765c4ee35e67.png',
+      imgLink: require('./img/todo_List.png'),
       siteLink:"https://todo-list-react-dun.vercel.app/",
-      githubLink:"#",
-      info:"Проект который........." 
+      githubLink:"https://github.com/Frostray1/todoListReact",
+      info:"Обычный to-do list \n Js, React, Tailwind CSS" 
     },
     {
-      title: 'To-do List',
-      imgLink: 'https://user-images.githubusercontent.com/48648751/215086300-1f7cc837-ddfb-4c9a-bc28-765c4ee35e67.png',
-      siteLink:"#",
-      githubLink:"#",
-      info:""
+      title: 'Weather App',
+      imgLink: require('./img/weatherApp.png'),
+      siteLink:"https://weather-yandex-api.vercel.app/",
+      githubLink:"https://github.com/Frostray1/weatherYandexAPI",
+      info: "Погодный информер \n API: Open-meteo, Nominatim \n HTML, CSS(Scss), JS"
     },
     {
-      title: 'To-do List',
-      imgLink: 'https://user-images.githubusercontent.com/48648751/215086300-1f7cc837-ddfb-4c9a-bc28-765c4ee35e67.png',
+      title: 'News app',
+      imgLink: require('./img/news.png'),
       siteLink:"#",
       githubLink:"#",
-      info:""
+      info:''
     },
     {
       title: 'To-do List',
